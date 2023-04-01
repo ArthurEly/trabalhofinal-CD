ff2afbf8e3b3b3c9f00cef3f76c96c206bee2fa2
ArthurEly <arthur.ely78@gmail.com>
2023-04-01 14:30:03 -0300

    adicionando a lógica pra já funcionar com os switchs da placa

--- simulation/qsim/work/trabalhofinal/_primary.vhd
+++ simulation/qsim/work/trabalhofinal/_primary.vhd
@@ -3,14 +3,22 @@ use verilog.vl_types.all;
 entity trabalhofinal is
     port(
         address_reg_out : out    vl_logic_vector(7 downto 0);
+        sw9             : in     vl_logic;
+        sw8             : in     vl_logic;
         confirm_address : in     vl_logic;
         clk             : in     vl_logic;
-        address         : in     vl_logic_vector(7 downto 0);
+        sw0             : in     vl_logic;
+        sw1             : in     vl_logic;
+        sw2             : in     vl_logic;
+        sw3             : in     vl_logic;
+        sw4             : in     vl_logic;
+        sw5             : in     vl_logic;
+        sw6             : in     vl_logic;
+        sw7             : in     vl_logic;
         data_out        : out    vl_logic_vector(7 downto 0);
         read            : in     vl_logic;
         write           : in     vl_logic;
         confirm_data    : in     vl_logic;
-        data            : in     vl_logic_vector(7 downto 0);
         data_reg_out    : out    vl_logic_vector(7 downto 0);
         morte           : out    vl_logic_vector(7 downto 0)
     );
--- simulation/qsim/work/trabalhofinal_vlg_sample_tst/_primary.vhd
+++ simulation/qsim/work/trabalhofinal_vlg_sample_tst/_primary.vhd
@@ -2,12 +2,20 @@ library verilog;
 use verilog.vl_types.all;
 entity trabalhofinal_vlg_sample_tst is
     port(
-        address         : in     vl_logic_vector(7 downto 0);
         clk             : in     vl_logic;
         confirm_address : in     vl_logic;
         confirm_data    : in     vl_logic;
-        data            : in     vl_logic_vector(7 downto 0);
         read            : in     vl_logic;
+        sw0             : in     vl_logic;
+        sw1             : in     vl_logic;
+        sw2             : in     vl_logic;
+        sw3             : in     vl_logic;
+        sw4             : in     vl_logic;
+        sw5             : in     vl_logic;
+        sw6             : in     vl_logic;
+        sw7             : in     vl_logic;
+        sw8             : in     vl_logic;
+        sw9             : in     vl_logic;
         write           : in     vl_logic;
         sampler_tx      : out    vl_logic
     );
